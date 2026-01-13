bd0b63d98a99f7b02d74802b964f983fa34ca708
Helberth <helberth12@gmail.com>
2026-01-13 16:32:30 -0500

    feat: add views.py with methods GET, POST, DELETE

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Reservation
+from .serializers import ReservationSerializer
 
-# Create your views here.
+class ReservationListCreateView(generics.ListCreateAPIView):
+    queryset = Reservation.objects.all().order_by('date', 'time')
+    serializer_class = ReservationSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class ReservationDeleteView(generics.DestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    #permission_classes = [permissions.IsAuthenticated]
